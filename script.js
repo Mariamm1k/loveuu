87c2e275c744388f55648e3066ad04397a8fbead
Mariam Kristesiashvili <96055515+Mariamm1k@users.noreply.github.com>
2024-01-24 05:48:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('loveMessage').addEventListener('click', function() {
+    // Replace the placeholder text with images or your interactive content
+    this.innerHTML = '<img src="loveis.png" alt="Girl and Boy Kissing">';
+});
